72a9837e2305122c96fbdb72f93a8f057efbec9f
flipboa <flipboa@gmail.com>
2025-06-02 15:35:28 +0800

    Site updated: 2025-06-02 15:35:28

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/02/My-New-Post/","2025/06/02/hello-world/"];function toRandomPost(){
+var posts=["2025/06/02/这是第二篇新的博文/","2025/06/02/My-New-Post/","2025/06/02/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
